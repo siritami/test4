8505b66e96e6f55738653389d23bced9960f7124
siritami <103488094+siritami@users.noreply.github.com>
2025-04-18 00:02:49 +0700

    Update Spotjfy.sh

--- src/build/Spotjfy.sh
+++ src/build/Spotjfy.sh
@@ -6,11 +6,12 @@ source src/build/utils.sh
 dl_gh "revanced-patches revanced-cli" "revanced" "latest"
 
 # Patch Spotjfy:
-patch_Spotjfy() {
-	get_patches_key "Spotjfy-revanced"
-	local j="i"
-	get_apkpure "com.spot"$j"fy.music" "spotjfy" "spot"$j"fy-music-and-podcasts-for-android/com.spot"$j"fy.music" "Bundle_extract"
-	split_editor "spotjfy" "spotjfy"
-	patch "spotjfy" "revanced"
-}
-patch_Spotjfy
+get_patches_key "Spotjfy-revanced"
+j="i"
+get_apkpure "com.spot"$j"fy.music" "spotjfy" "spot"$j"fy-music-and-podcasts-for-android/com.spot"$j"fy.music" "Bundle_extract"
+split_editor "spotjfy" "spotjfy"
+patch "spotjfy" "revanced"
+# Patch Spotjfy Arm64-v8a
+get_patches_key "Spotjfy-revanced"
+split_editor "spotjfy" "spotjfy-arm64-v8a" "exclude" "split_config.armeabi_v7a split_config.x86 split_config.x86_64"
+patch "spotjfy-arm64-v8a" "revanced"
