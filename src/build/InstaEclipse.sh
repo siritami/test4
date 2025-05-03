593c608af71de7442ca61d3d6550b53771a0ac9b
siritami <103488094+siritami@users.noreply.github.com>
2025-05-03 20:10:20 +0700

    Update InstaEclipse.sh

--- src/build/InstaEclipse.sh
+++ src/build/InstaEclipse.sh
@@ -7,5 +7,5 @@ dl_gh "InstaEclipse" "ReSo7200" "latest"
 dl_gh "LSPatch" "JingMatrix" "latest" "manager.apk"
 
 # Patch Instgram Arm64-v8a
-#get_apk "com.instagram.android" "instagram-arm64-v8a" "instagram-instagram" "instagram/instagram-instagram/instagram" "arm64-v8a" "nodpi"
+get_apk "com.instagram.android" "instagram-arm64-v8a" "instagram-instagram" "instagram/instagram-instagram/instagram" "arm64-v8a" "nodpi"
 lspatch "instagram-arm64-v8a" "InstaEclipse"
